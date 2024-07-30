6c0934d495f68b391def9ad34d55fb53acd7f7b7
KhoiUna <thorwaitson@gmail.com>
2024-07-30 09:01:21 -0500

    update app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,46 +1,46 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { BRAND_NAME, BRAND_URL } from './config'
-import Analytics from '@/components/Analytics'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { BRAND_NAME, BRAND_URL } from './config';
+import Analytics from '@/components/analytics';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
-const description = 'A place to talk ideas for building a utopian future.'
+const description = 'A place to talk ideas for building a utopian future.';
 export const metadata: Metadata = {
-  title: {
-    default: `Home | ${BRAND_NAME}`,
-    template: `%s | ${BRAND_NAME}`
-  },
-  description,
-  metadataBase: new URL(BRAND_URL),
-  openGraph: {
-    title: `Home | ${BRAND_NAME}`,
-    description,
-    url: BRAND_URL,
-    siteName: `${BRAND_NAME}`,
-    images: [
-      {
-        url: '/images/og.png',
-        width: 512,
-        height: 512,
-      },
-    ],
-    locale: 'en-US',
-    type: 'website',
-  }
-}
+	title: {
+		default: `Home | ${BRAND_NAME}`,
+		template: `%s | ${BRAND_NAME}`,
+	},
+	description,
+	metadataBase: new URL(BRAND_URL),
+	openGraph: {
+		title: `Home | ${BRAND_NAME}`,
+		description,
+		url: BRAND_URL,
+		siteName: `${BRAND_NAME}`,
+		images: [
+			{
+				url: '/images/og.png',
+				width: 512,
+				height: 512,
+			},
+		],
+		locale: 'en-US',
+		type: 'website',
+	},
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <Analytics />
+	return (
+		<html lang='en'>
+			<Analytics />
 
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,67 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
-export default function page() {
-  return (
-    <main className='min-h-screen p-5 pt-0'>
-      <Image className='m-auto' src={'/images/platopunk.webp'} alt='Logo' width={90} height={90} />
-      <h1 className='text-center text-2xl'>PlatoPunk</h1>
-      <h2 className='text-center text-xl font-light mt-1 italic'>Open Minds. Open Source. Crafting Utopian Futures.</h2>
-      <h3 className='max-w-[400px] text-center text-lg font-light mt-3 mx-auto'>PlatoPunk is a platform for people who are passionate about sharing/using/building (open-source) tech/art that crafts a better future.</h3>
+export default function Page() {
+	return (
+		<main className='min-h-screen p-5 pt-0'>
+			<Image
+				className='m-auto'
+				src={'/images/platopunk.webp'}
+				alt='Logo'
+				width={90}
+				height={90}
+			/>
+			<h1 className='text-center text-2xl'>PlatoPunk</h1>
+			<h2 className='text-center text-xl font-light mt-1 italic'>
+				Open Minds. Open Source. Crafting Utopian Futures.
+			</h2>
+			<h3 className='max-w-[400px] text-center text-lg font-light mt-3 mx-auto'>
+				PlatoPunk is a platform for people who are passionate about
+				sharing/using/building (open-source) tech/art that crafts a
+				better future.
+			</h3>
 
-      <div className='pt-20'>
-        <Image className='w-[150px] m-auto rounded-full' priority src={'/images/plato.jpg'} alt='Plato' width={500} height={500} />
-        <p className='text-center font-light italic mt-3'>
-          {`"The beginning is the most important part of the work."`}
-          <br />
-          ― Plato, The Republic
-        </p>
+			<div className='pt-20'>
+				<Image
+					className='w-[150px] m-auto rounded-full'
+					priority
+					src={'/images/plato.jpg'}
+					alt='Plato'
+					width={500}
+					height={500}
+				/>
+				<p className='text-center font-light italic mt-3'>
+					{`"The beginning is the most important part of the work."`}
+					<br />― Plato, The Republic
+				</p>
 
-        <div className='mt-8 text-center'>
-          <h3 className='text-xl mb-3 underline underline-offset-4'>Manifesto</h3>
-          <ul className='text-lg font-light'>
-            <li className='py-1'>No spams. No bots.</li>
-            <li className='py-1'>No dopamine. No ads.</li>
-            <li className='py-1'>
-              <a className='underline underline-offset-4' href="https://github.com/KhoiUna/platopunk" target='_blank'>Open source</a>. Transparent.
-            </li>
-          </ul>
-        </div>
+				<div className='mt-8 text-center'>
+					<h3 className='text-xl mb-3 underline underline-offset-4'>
+						Manifesto
+					</h3>
+					<ul className='text-lg font-light'>
+						<li className='py-1'>No spams. No bots.</li>
+						<li className='py-1'>No dopamine. No ads.</li>
+						<li className='py-1'>
+							<a
+								className='underline underline-offset-4'
+								href='https://github.com/KhoiUna/platopunk'
+								target='_blank'
+							>
+								Open source
+							</a>
+							. Transparent.
+						</li>
+					</ul>
+				</div>
 
-        <div className="text-center mt-10">
-          <a href="https://tally.so/r/w8aqXl" target='_blank'>
-            <button className='font-bold border-2 border-black p-2 drop-shadow-lg rounded-lg bg-white ease-in-out transition-all hover:bg-black hover:text-white'>
-              Join Waitlist
-            </button>
-          </a>
-        </div>
-      </div>
-    </main>
-  )
+				<div className='text-center mt-10'>
+					<a href='https://tally.so/r/w8aqXl' target='_blank'>
+						<button className='font-bold border-2 border-black p-2 drop-shadow-lg rounded-lg bg-white ease-in-out transition-all hover:bg-black hover:text-white'>
+							Join Waitlist
+						</button>
+					</a>
+				</div>
+			</div>
+		</main>
+	);
 }
