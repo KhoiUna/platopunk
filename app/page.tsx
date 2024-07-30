1b7b1645db3211dd94ce95b7f18c8d7123e7e632
Khoi Nguyen <63745633+KhoiUna@users.noreply.github.com>
2024-07-30 13:34:02 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Page() {
 		<div className='p-4'>
 			<h1 className='text-2xl sm:text-4xl font-thin pt-20 max-w-3xl leading-loose'>
 				<b>PlatoPunk</b> is a software development studio trying to ship
-				<i className='px-2 underline'>
+				<i className='px-2 underline decoration-2 underline-offset-8'>
 					minimalistic software
 				</i>{' '}
 				to the world.
